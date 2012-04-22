159489d6a8ba0e73bb0866c48ee7d8c722c012c2
John Wells <John.Wells@utah.edu>
2012-04-22 02:25:36 -0600

    Changes to world loader, comments are included.

--- scripts/worldloader.lua
+++ scripts/worldloader.lua
@@ -17,27 +17,62 @@
    2- That file's table entires are iterated through.
    3- Using add_atoms, the nested tables are added into the heirarchy.
    4- At the bottom of each nesting, there are base things/containers with flavor text descriptions. These are then created and placed into the game.
+
+   Joel's comments:
+
+   I think, for the world builder, it would be easier if you split type making and instance making into two parts. 
+   It seems like it will be difficult later to set up room layout and put things in them with this setup.  
+   Also, the atoms are getting added but the relationships are not.  
+   The function layout and thought process look fine, though.
+
+
+   I WISH I COULD DO:
+   Any amount of table entries, from things to character classes to emotes.
+   Plain english (Bartle-ized) world building. Example: There is a dirk that is a type of sword
+   Creating links between rooms (using portals)
+   Creating AI
+
    --]]
 
 
 function buildWorld(file)
    dofile(file)
    local types = { Things, Containers }  -- CURRENT EXPECTED FORMAT FOR INPUT FILE
-   function worldHelper(t)
-      for k,v in pairs(t) do
-         if (type(v) == "table") then
-            DEBUG("Adding: " .. tostring(k).lower(k))
-	    add_atoms(k)
-            table.insert(printed, v)
-            worldHelper(v)
-	 else
-	    DEBUG(tostring(k).lower(k) .. ' description '  .. '=' .. v)
-	    create_thing(tostring(k).lower(k), { description = v } ) --Create the first instance of the thing. 
-	 end
-      end
-   end
+   local instances = { } -- Table housing the actual instances of the things created. 
+
    for k,v in pairs(types) do
       worldHelper(v);
    end
+   instanceBuilder(instances)
 end
- 
+
+function typeBuilder(t, parent)
+   for k,v in pairs(t) do
+      if (type(v) == "table") then
+	 DEBUG("Adding: " .. tostring(k).lower(k))
+	 add_atom(tostring(k).lower(k))
+	 if parent then
+	    add_parent(tostring(k).lower(k), parent)
+	    worldHelper(v, tostring(k).lower(k))
+	 else
+	    worldHelper(v, k)
+	 end
+	 
+      else
+	 DEBUG(tostring(k).lower(k) .. ' description '  .. '=' .. v)
+
+	 -- Instance Handling:
+	 table.insert(instances, k)
+      end
+   end
+end
+
+
+-- Uses add_thing to create instances of stuff in the game. Doesn't really do anything else. At this point, the muderator needs to make stuff 
+function instanceBuilder(instances)
+   for k,v in pairs(instances) do
+      if add_thing(k, v) then
+	 DEBUG("Successfully added " .. k .. " into the game.")
+      end
+   end   
+end
