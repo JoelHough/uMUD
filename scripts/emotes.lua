4370b5c2f6789c411bc3f89e77f608d35090f135
John Wells <John.Wells@utah.edu>
2012-04-21 18:00:22 -0600

    Initial build of World Loader, needs testing, but my Lpeg libraries are jacked... again. The idea is that it iterates through the table provided from the included file, adds the tables to the atoms list, and then gives them descriptions based on the flavor text in the file.

---
+++ scripts/emotes.lua
@@ -0,0 +1,236 @@
+agree$You agree.$You agree with <target>.
+amaze$You are amazed!$You are amazed by <target>.
+angry$You raise your fist in anger.$You raise your fist in anger at <target>.
+apologize$You apologize to everyone
+attacktarget$You tell everyone to attack something.$You tell everyone to attack <target>.
+bark$You bark. Woof woof!$You bark at <target>
+bashful$You are bashful.$You are so bashful...too bashful to get <target>'s attention.
+beckon$You beckon everyone over to you.$You beckon <target> over.
+beg$You beg everyone around you. How pathetic.$You beg <target>. How pathetic.
+belch$You let out a loud belch.$You burp rudely in <target>'s face.
+bite$You look around for someone to bite.$You bite <target>. Ouch!
+bleed
+blood
+Blood oozes from your wounds.
+blink$You blink your eyes.$You blink at <target>.
+blow$You blow a kiss into the wind.$You blow a kiss to <target>.
+blush$You blush.$You blush at <target>.
+boggle$You boggle at the situation.$You boggle at <target>.
+bonk$You bonk yourself on the noggin. Doh!$You bonk <target> on the noggin. Doh!
+bored$You are overcome with boredom. Oh the drudgery!$You are terribly bored with <target>.
+bounce$You bounce up and down.$You bounce up and down in front of <target>.
+bow$You bow down graciously.$You bow before <target>.
+bravo$You applaud. Bravo!$You applaud at <target>. Bravo!
+brb$You let everyone know you'll be right back$You let <target> know you'll be right back.
+burp$You let out a loud belch.$You burp rudely in <target>'s face.
+bye$You wave goodbye to everyone. Farewell!$You wave goodbye to <target>. Farewell!
+cackle$You cackle maniacally at the situation.$You cackle maniacally at <target>.
+calm$You remain calm.$You try to calm <target> down.
+cat
+catty
+You scratch yourself. Ah, much better!$You scratch <target>. How catty!
+charge$You start to charge.$
+cheer$You cheer!$You cheer at <target>.
+chew$You begin to eat.$You begin to eat in front of <target>.
+chicken$With arms flapping, you strut around. Cluck, Cluck, Chicken!$With arms flapping, you strut around <target>. Cluck, Cluck, Chicken!
+chuckle$You let out a hearty chuckle.$You chuckle at <target>.
+clap$You clap excitedly.$You clap excitedly for <target>.
+cold$You let everyone know that you are cold.$You let <target> know that you are cold.
+comfort$You need to be comforted.$You comfort <target>.
+commend$You commend everyone on a job well done.$You commend <target> on a job well done.
+confused$You are hopelessly confused.$You look at <target> with a confused look.
+congrats, cong
+congratulate
+ You congratulate everyone around you.$You congratulate <target>.
+cough$You let out a hacking cough.$You cough at <target>.
+cower$You cower in fear.$You cower in fear at the sight of <target>.
+crack$You crack your knuckles.$You crack your knuckles while staring at <target>.
+cringe$You cringe in fear.$You cringe away from <target>.
+cry$You cry.$You cry on <target>'s shoulder.
+cuddle$You need to be cuddled.$You cuddle up against <target>.
+curious$You express your curiosity to those around you.$You are curious what <target> is up to.
+curtsey$You curtsey.$You curtsey before <target>.
+dance$You burst into dance.$You dance with <target>.
+disappointed
+disappointment
+You frown.$You frown with disappointment at <target>.
+doh$You bonk yourself on the noggin. Doh!$You bonk <target> on the noggin. Doh!
+doom$You threaten everyone with the wrath of doom.$You threaten <target> with the wrath of doom.
+drink$You raise a drink in the air before chugging it down. Cheers!$You raise a drink to <target>. Cheers!
+drool$A tendril of drool runs down your lip.$You look at <target> and begin to drool.
+duck$You duck for cover.$You duck behind <target>.
+eat$You begin to eat.$You begin to eat in front of <target>.
+excited$You talk excitedly with everyone.$You talk excitedly with <target>.
+eye$You cross your eyes.$You eye <target> up and down.
+facepalm$You cover your face with your palm.$You look over at <target> and cover your face with your palm.
+farewell$You wave goodbye to everyone. Farewell!$You wave goodbye to <target>. Farewell!
+fart$You fart loudly. Whew...what stinks?$You brush up against <target> and fart loudly.
+fear$You cower in fear.$You cower in fear at the sight of <target>.
+feast$You begin to eat.$You begin to eat in front of <target>.
+fidget$You fidget.$You fidget impatiently while waiting for <target>.
+flap$With arms flapping, you strut around. Cluck, Cluck, Chicken!$With arms flapping, you strut around <target>. Cluck, Cluck, Chicken!
+flee$You yell for everyone to flee!$You yell for <target> to flee!
+fle $You flex your muscles. Oooooh so strong!$You flex at <target>. Oooooh so strong!
+flirt$You flirt.$You flirt with <target>.
+flop$You flop about helplessly.$You flop about helplessly around <target>.
+followme$You motion for everyone to follow.$You motion for <target> to follow.
+food$You are hungry!$You are hungry. Maybe <target> has some food...
+frown$You frown.$You frown with disappointment at <target>.
+gasp$You gasp.$You gasp at <target>.
+gaze$You gaze off into the distance.$You gaze eagerly at <target>.
+giggle$You giggle.$You giggle at <target>.
+glad$You are filled with happiness!$You are very happy with <target>!
+glare$You glare angrily.$You glare angrily at <target>.
+gloat$You gloat over everyone's misfortune.$You gloat over <target>'s misfortune.
+golfclap$You clap half heartedly, clearly unimpressed.$You clap for <target>, clearly unimpressed.
+goodbye$You wave goodbye to everyone. Farewell!$You wave goodbye to <target>. Farewell!
+greet
+greetings
+$You greet everyone warmly.$You greet <target> warmly.
+grin$You grin wickedly.$You grin wickedly at <target>.
+groan$You begin to groan.$You look at <target> and groan.
+grovel$You grovel on the ground, wallowing in subservience.$You grovel before <target> like a subservient peon.
+growl$You growl menacingly.$You growl menacingly at <target>.
+guffaw$You let out a boisterous guffaw!$You take one look at <target> and let out a guffaw!
+hail$You hail those around you.$You hail <target>.
+happy$You are filled with happiness!$You are very happy with <target>!
+healme$You call out for healing!$
+hello$You greet everyone with a hearty hello!$You greet <target> with a hearty hello!
+helpme$You cry out for help!$
+hi$You greet everyone with a hearty hello!$You greet <target> with a hearty hello!
+highfive$You put up your hand for a high five.$You give <target> a high five!
+hug$You need a hug!$You hug <target>.
+hungry$You are hungry!$You are hungry. Maybe <target> has some food...
+impatient$You fidget.$You fidget impatiently while waiting for <target>.
+incoming$You warn everyone of incoming enemies!$You point out <target> as an incoming enemy!
+insult$You think everyone around you is a son of a motherless ogre.$You think <target> is the son of a motherless ogre.
+introduce$You introduce yourself to everyone.$You introduce yourself to <target>.
+jk$You were just kidding!$You let <target> know that you were just kidding!
+kiss$You blow a kiss into the wind.$You blow a kiss to <target>.
+kneel$You kneel down.$You kneel before <target>.
+knuckles$You crack your knuckles.$You crack your knuckles while staring at <target>.
+laugh$You laugh.$You laugh at <target>.
+lavish$You praise the Light.$You lavish praise upon <target>.
+lay
+laydown
+$You lie down.$You lie down before <target>.
+lick$You lick your lips.$You lick <target>.
+lie
+liedown
+$You lie down.$You lie down before <target>.
+listen$You are listening!$You listen intently to <target>.
+lol$You laugh.$You laugh at <target>.
+lost$You are hopelessly lost.$You want <target> to know that you are hopelessly lost.
+love$You feel the love.$You love <target>.
+mad$You raise your fist in anger.$You raise your fist in anger at <target>.
+massage$You need a massage!$You massage <target>'s shoulders.
+moan$You moan suggestively.$You moan suggestively at <target>.
+mock$You mock life and all it stands for.$You mock the foolishness of <target>.
+moo$Tauren only$Mooooooooooo.$You moo at <target>. Mooooooooooo.
+moon$You drop your trousers and moon everyone.$You drop your trousers and moon <target>.
+mountspecial$Rear-up animation for all mounts (including flying). Works only when mounted$
+mourn$In quiet contemplation, you mourn the loss of the dead.$In quiet contemplation, you mourn the death of <target>.
+no$You clearly state, NO.$You tell <target> NO. Not going to happen.
+nod$You nod.$You nod at <target>.
+nosepick$With a finger deep in one nostril, you pass the time.$You pick your nose and show it to <target>.
+oom$You announce that you have low mana!
+openfire$You give the order to open fire.$
+panic$You run around in a frenzied state of panic.$You take one look at <target> and panic.
+party$You raise a drink in the air before chugging it down. Cheers!$You raise a drink to <target>. Cheers!
+pat$You need a pat.$You gently pat <target>.
+peer$You peer around, searchingly.$You peer at <target> searchingly.
+peon$You grovel on the ground, wallowing in subservience.$You grovel before <target> like a subservient peon.
+pest$You shoo the measly pests away.$You shoo <target> away. Be gone pest!
+pick$With a finger deep in one nostril, you pass the time.$You pick your nose and show it to <target>.
+pity$You pity those around you.$You look down upon <target> with pity.
+pizza$You are hungry!$You are hungry. Maybe <target> has some food...
+plead$You drop to your knees and plead in desperation.$You plead with <target>.
+point$You point over yonder.$You point at <target>.
+poke$You poke your belly and giggle.$You poke <target>. Hey!
+ponder$You ponder the situation.$You ponder <target>'s actions.
+pounce$You pounce out from the shadows.$You pounce on top of <target>.
+praise$You praise the Light.$You lavish praise upon <target>.
+pray$You pray to the Gods.$You say a prayer for <target>.
+purr$You purr like a kitten.$You purr at <target>.
+puzzled$You are puzzled. What's going on here?$You are puzzled by <target>.
+question$You want to know the meaning of life.$You question <target>.
+raise$You raise your hand in the air.$You look at <target> and raise your hand.
+rasp$You make a rude gesture.$You make a rude gesture at <target>.
+rdy
+ready
+You let everyone know that you are ready!$You let <target> know that you are ready!
+rear$You shake your rear.$You shake your rear at <target>.
+roar$You roar with bestial vigor. So fierce!$You roar with bestial vigor at <target>. So fierce!
+rofl$You roll on the floor laughing.$You roll on the floor laughing at <target>.
+rude$You make a rude gesture.$You make a rude gesture at <target>.
+salute$You stand at attention and salute.$You salute <target> with respect.
+scared$You are scared!$You are scared of <target>.
+scratch$You scratch yourself. Ah, much better!$You scratch <target>. How catty!
+sexy$You're too sexy for your tunic...so sexy it hurts.$You think <target> is a sexy devil.
+shake$You shake your rear.$You shake your rear at <target>.
+shimmy$You shimmy before the masses.$You shimmy before <target>.
+shindig$You raise a drink in the air before chugging it down. Cheers!$You raise a drink to <target>. Cheers!
+shiver$You shiver in your boots. Chilling!$You shiver beside <target>. Chilling!
+shoo$You shoo the measly pests away.$You shoo <target> away. Be gone pest!
+shrug$You shrug. Who knows?$You shrug at <target>. Who knows?
+shy$You smile shyly.$You smile shyly at <target>.
+sigh$You let out a long, drawn-out sigh.$You sigh at <target>.
+silly$You tell a joke.$You tell <target> a joke.
+slap$You slap yourself across the face. Ouch!$You slap <target> across the face. Ouch!
+sleep$You fall asleep. Zzzzzzz.$You fall asleep. Zzzzzzz.
+smell$You smell the air around you. Wow, someone stinks!$You smell <target>. Wow, someone stinks!
+smile$You smile.$You smile at <target>.
+smirk$A sly smirk spreads across your face.$You smirk slyly at <target>.
+snarl$You bare your teeth and snarl.$You bare your teeth and snarl at <target>.
+snicker$You quietly snicker to yourself.$You snicker at <target>.
+sniff$You sniff the air around you.$You sniff <target>.
+snub$You snub all of the lowly peons around you.$You snub <target>.
+sob$You cry.$You cry on <target>'s shoulder.
+soothe$You need to be soothed.$You soothe <target>. There, there...things will be ok.
+sorry$You apologize to everyone. Sorry!$You apologize to <target>. Sorry!
+spit$You spit on the ground.$You spit on <target>.
+spoon$You need to be cuddled.$You cuddle up against <target>.
+stare$You stare off into the distance.$You stare <target> down.
+stink$You smell the air around you. Wow, someone stinks!$You smell <target>. Wow, someone stinks!
+strong$You flex your muscles. Oooooh so strong!$You flex at <target>. Oooooh so strong!
+strut$With arms flapping, you strut around. Cluck, Cluck, Chicken!$With arms flapping, you strut around <target>. Cluck, Cluck, Chicken!
+surprised$You are so surprised!$You are surprised by <target>'s actions.
+surrender$You surrender to your opponents.$You surrender before <target>. Such is the agony of defeat...
+talk$You talk to yourself since no one else seems interested$You want to talk things over with <target>
+talke $You talk excitedly with everyone$You talk excitedly with <target>
+talkq$You want to know the meaning of life.$You question <target>.
+tap$You tap your foot. Hurry up already!$You tap your foot as you wait for <target>.
+taunt$You taunt everyone around you. Bring it fools!$You make a taunting gesture at <target>. Bring it!
+tease$You are such a tease.$You tease <target>.
+thank
+thanks
+ You thank everyone around you.$You thank <target>.
+thirsty$You are so thirsty. Can anyone spare a drink?$You let <target> know you are thirsty. Spare a drink?
+threat
+threaten
+You threaten everyone with the wrath of doom.$You threaten <target> with the wrath of doom.
+tickle$You want to be tickled. Hee hee!$You tickle <target>. Hee hee!
+tired$You let everyone know that you are tired.$You let <target> know that you are tired.
+train$"Choo Choo Train" animation and sound$
+ty$You thank everyone around you.$You thank <target>.
+veto$You veto the motion on the floor.$You veto <target>'s motion.
+victory$You bask in the glory of victory.$You bask in the glory of victory with <target>.
+violin$You begin to play the world's smallest violin.$You play the world's smallest violin for <target>.
+volunteer$You raise your hand in the air.$You look at <target> and raise your hand.
+wait$You ask everyone to wait.$<target> asks <target> to wait.
+wave$You beckon everyone over to you.$You wave at <target>.
+weep$You cry.$You cry on <target>'s shoulder.
+welcome$You welcome everyone.$You welcome <target>.
+whine$You whine pathetically.$You whine pathetically at <target>.
+whistle$You let forth a sharp whistle.$You whistle at <target>.
+wicked
+wickedly
+You grin wickedly.$You grin wickedly at <target>.
+wink$You wink slyly.$You wink slyly at <target>.
+woot$You cheer!$You cheer at <target>.
+work$You begin to work.$You work with <target>.
+wrath$You threaten everyone with the wrath of doom.$You threaten <target> with the wrath of doom.
+yawn$You yawn sleepily.$You yawn sleepily at <target>.
+yay$You are filled with happiness!$You are very happy with <target>!
+yes$You nod.$You nod at <target>.
+yw$You were happy to help.$You were happy to help <target>.e. Sorry!$You apologize to <target>. Sorry!
