e34a1c820693f981f08d88413a0e4e80713faca7
Joel Hough <joel@joelhough.com>
2012-04-23 03:52:30 -0600

    It works!

--- src/umud_bot.cpp
+++ src/umud_bot.cpp
@@ -44,6 +44,9 @@ using namespace std;
 #define PORT "6667" // the port client will be connecting to
 #define IRCHOST "irc.freenode.net"
 
+const string UMUD_PORT = "4000";
+const string UMUD_HOST = "umud.hyob.net";
+
 #define MAXDATASIZE 500 // max number of bytes we can get at once
 
 const string room = "#TeamException";
@@ -65,7 +68,11 @@ fd_set out_set;
 
 struct IrcPlayer {
   int socket;
+
+  // These aren't neccessarily the same!  irc is more tolerant than umud
   string name;
+  string nick;
+
   string out_buf;
   string in_buf;
 };
@@ -152,52 +159,52 @@ bool read_into_buffer(int &from_socket, string &to_buf)
   return true;
 }
 
-bool connect_to_irc(const string &host)
+bool connect_with_nonblocking(int &sock, const string &host, const string &port)
 {
   struct addrinfo hints, *servinfo, *p;
-    int rv;
-    char s[INET6_ADDRSTRLEN];
+  int rv;
+  char s[INET6_ADDRSTRLEN];
 
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
 
-    if ((rv = getaddrinfo(host.c_str(), PORT, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return false;
+  if ((rv = getaddrinfo(host.c_str(), port.c_str(), &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return false;
+  }
+
+  // loop through all the results and connect to the first we can
+  for(p = servinfo; p != NULL; p = p->ai_next) {
+    if ((sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+      perror("client: socket");
+      continue;
     }
-
-    // loop through all the results and connect to the first we can
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((ircSocket = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-            perror("client: socket");
-            continue;
-        }
-        if (connect(ircSocket, p->ai_addr, p->ai_addrlen) == -1) {
-            close(ircSocket);
-            perror("client: connect");
-            continue;
-        }
-        break;
+    if (connect(sock, p->ai_addr, p->ai_addrlen) == -1) {
+      close(sock);
+      perror("client: connect");
+      continue;
     }
+    break;
+  }
 
-    if (p == NULL) {
-        fprintf(stderr, "client: failed to connect\n");
-        return false;
-    }
+  if (p == NULL) {
+    fprintf(stderr, "client: failed to connect\n");
+    return false;
+  }
 
-    // Get some ASIO up in here
-    if (fcntl( ircSocket, F_SETFL, FNDELAY ) == -1) {
-      perror("client: fndelay");
-      return false;
-    }
+  // Get some ASIO up in here
+  if (fcntl( sock, F_SETFL, FNDELAY ) == -1) {
+    perror("client: fndelay");
+    return false;
+  }
 
-    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+  inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
             s, sizeof s);
-    printf("client: connecting to %s\n", s);
+  printf("client: connecting to %s\n", s);
 
-    freeaddrinfo(servinfo); // all done with this structure
-    return true;
+  freeaddrinfo(servinfo); // all done with this structure
+  return true;
 }
 
 void irc_send_line(string line)
@@ -234,7 +241,8 @@ void me_say_to_room(string text)
 
 void irc_quit()
 {
-  irc_send_line("QUIT :Peace out, bitches!");
+  say_to_room("Peace out, bitches!");
+  irc_send_line("QUIT :Like tears in the rain, time to die.");
 }
 
 void close_comms()
@@ -252,7 +260,7 @@ void close_comms()
 void bailout (int sig)
 {
   if (isQuitting) {
-    // Be more forceful
+    // Be more forceful the second time through here
     isRunning = false;
   } else {
     irc_quit();
@@ -260,16 +268,52 @@ void bailout (int sig)
   }
 }
 
+IrcPlayer *get_player_from_nick(string nick)
+{
+  for (PlayerListIterator i = playerList.begin(); i != playerList.end(); ++i) {
+    if ((*i)->socket != NO_SOCKET) {
+      if (boost::iequals((*i)->nick, nick)) {
+        return (*i);
+      }
+    }
+  }  
+  return NULL;
+}
+
+bool add_player(string nick)
+{
+  say_to(nick, "Would you like to play a game?");
+  say_to(nick, "How about global thermonuclear war?");
+  say_to(nick, "Or how about some MF'n uMUD!?");
+  int sock;
+  if (!connect_with_nonblocking(sock, UMUD_HOST, UMUD_PORT)) {
+    say_to(nick, "Or not, cause the uMUD server rejected you.");
+    cout << "Couldn't connect to uMUD" << endl;
+    return false;
+  }
+  IrcPlayer *player = new IrcPlayer();
+  player->nick = nick;
+  player->socket = sock;
+  playerList.push_front(player);
+  return true;
+}
+
 void process_room_umud_command(string command, string nick)
 {
   if (boost::iequals(command, "play")) {
     // Initiate a game of umud in a private room
     say_to_room("Do something with sockets one day.");
-    say_to(nick, "Would you like to play a game?  How about global thermonuclear war?");
+    add_player(nick);
   } else if (boost::starts_with(command, "/me ")) {
     // Dance for the people!
     me_say_to_room(command.substr(4));
   }
+}
+
+void process_player_command(IrcPlayer *player, string command)
+{
+  // TODO: Handle ACTION->emote conversion
+  player->out_buf += command + "\n";
 }
 
 void irc_process_line(string line)
@@ -282,7 +326,6 @@ void irc_process_line(string line)
     command = result.str("command");
     params = result.str("params");
     trail = result.str("trail");
-    cout << "(" << prefix << ")(" << command << ")(" << params << ")(" << trail << ")" << endl;
   } else {
     // Malformed message from the server.  This should not happen.
     cout << "No regex match!" << endl;
@@ -311,15 +354,23 @@ void irc_process_line(string line)
           umud_command = string(result[1].first, result[1].second);
           process_room_umud_command(umud_command, nick);
         }
-      } else {
+      } else { // Prolly equals my nick, don't bother checking (famous last words)
         // From somewhere else.  Perhaps from the nick themself?
+        IrcPlayer *player = get_player_from_nick(nick);
+        if (player) {
+          process_player_command(player, trail);
+        } else {
+          say_to(nick, "You aren't connected to the game!  Go to " + room + " and ask to play.");
+        }
       }
     } else {
       cout << "Can't find a nick on this msg" << endl;
     }
   } else {
-    cout << "^Don't know what to do with that line^" << endl;
+    cout << "Ignoring " << "(" << prefix << ")(" << command << ")(" << params << ")(" << trail << ")" << endl;
+    return;
   }
+  cout << "(" << prefix << ")(" << command << ")(" << params << ")(" << trail << ")" << endl;
 }
 
 void irc_process_lines()
@@ -330,50 +381,112 @@ void irc_process_lines()
   }
 }
 
+void player_process_line(IrcPlayer *player, string line)
+{
+  say_to(player->nick, line);
+}
+
+void player_process_lines(IrcPlayer *player)
+{
+  string line;
+  while (get_line(player->in_buf, line)) {
+    player_process_line(player, line);
+  }  
+}
+
+void players_process_lines()
+{
+  for (PlayerListIterator i = playerList.begin(); i != playerList.end(); ++i) {
+    if ((*i)->socket != NO_SOCKET) {
+      player_process_lines((*i));
+    }
+  }
+}
+
 void remove_disconnected_players()
 {
-  // TODO:
+  PlayerListIterator i = playerList.begin ();
+  while (i != playerList.end ()) {
+    IrcPlayer *player = (*i);
+    PlayerListIterator prev = i;
+    i++;
+    if (player->socket == NO_SOCKET) {
+      say_to(player->nick, "You are now disconnected from uMUD");
+      playerList.erase(prev);
+      delete player;
+      player = 0;
+    }
+  } /* end of looping through players */
+}
+
+int set_descriptors()
+{
+  FD_ZERO(&in_set);
+  FD_ZERO(&out_set);
+
+  int max_socket = ircSocket;
+  
+  // We want IO from irc
+  FD_SET(ircSocket, &in_set);
+  if (!ircOutBuf.empty()) {
+    FD_SET(ircSocket, &out_set);
+  }
+  
+  // And IO from players
+  for (PlayerListIterator i = playerList.begin(); i != playerList.end(); ++i) {
+    if ((*i)->socket != NO_SOCKET) {
+      max_socket = max ((*i)->socket, max_socket);
+      FD_SET((*i)->socket, &in_set);
+      if (!(*i)->out_buf.empty()) {
+        FD_SET((*i)->socket, &out_set);
+      }
+    }
+  }  
+  return max_socket;
+}
+
+void fill_buffers()
+{
+  if (FD_ISSET(ircSocket, &in_set)) {
+    // Handle the irc happenings
+    read_into_buffer(ircSocket, ircInBuf);
+  }
+  if (FD_ISSET(ircSocket, &out_set)) {
+    // Send that irc shit out!
+    write_from_buffer(ircSocket, ircOutBuf);
+  }
+
+  // Loop through the players, checking the status, handling the buffers
+  for (PlayerListIterator i = playerList.begin(); i != playerList.end(); ++i) {
+    if ((*i)->socket != NO_SOCKET) {
+      if (FD_ISSET((*i)->socket, &in_set)) {
+        read_into_buffer((*i)->socket, (*i)->in_buf);
+      }
+      if (FD_ISSET((*i)->socket, &out_set)) {
+        write_from_buffer((*i)->socket, (*i)->out_buf);
+      }
+    }
+  }
 }
 
 void io_loop()
 {
+  // select timeout
+  struct timeval timeout;
+  timeout.tv_sec = 0;
+  timeout.tv_usec = 500 * 1000;
+
   cout << "Entering io loop" << endl;
   while (isRunning) {
-    FD_ZERO(&in_set);
-    FD_ZERO(&out_set);
 
-    // We want IO from irc
-    FD_SET(ircSocket, &in_set);
-    if (!ircOutBuf.empty()) {
-      FD_SET(ircSocket, &out_set);
-    }
-    // And IO from players
-    // TODO:
-
-    // Get max
-    // TODO:
-    int max_socket = ircSocket;
-
-    // select timeout
-    struct timeval timeout;
-    timeout.tv_sec = 0;
-    timeout.tv_usec = 500 * 1000;
+    int max_socket = set_descriptors();
 
     if (select(max_socket + 1, &in_set, &out_set, NULL, &timeout) > 0) {
-      if (FD_ISSET(ircSocket, &in_set)) {
-        // Handle the irc happenings
-        read_into_buffer(ircSocket, ircInBuf);
-      }
-      if (FD_ISSET(ircSocket, &out_set)) {
-        // Send that irc shit out!
-        write_from_buffer(ircSocket, ircOutBuf);
-      }
-
-      // Loop through the players, checking the status
-      // TODO:
+      fill_buffers();
     }
 
     irc_process_lines();
+    players_process_lines();
 
     remove_disconnected_players();
   }
@@ -393,7 +506,7 @@ int main(int argc, char *argv[])
   signal (SIGTERM, bailout);
   signal (SIGHUP,  bailout);
 
-  if (!connect_to_irc(argv[1])) {
+  if (!connect_with_nonblocking(ircSocket, argv[1], PORT)) {
     return -1;
   }
 
